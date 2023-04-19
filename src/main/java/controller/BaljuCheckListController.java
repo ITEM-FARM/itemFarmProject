3a8779247591038fc56f22c77a4b1effcdc71568
teon98 <k55416325@gmail.com>
2023-04-19 20:19:35 +0900

    FEAT: 발주상품checklist 기능 구현

---
+++ src/main/java/controller/BaljuCheckListController.java
@@ -0,0 +1,34 @@
+package controller;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import frontcontroller.CommonInterface;
+import model.BaljuService;
+import vo.ProductVO;
+
+//[태영] 
+public class BaljuCheckListController implements CommonInterface {
+
+	@Override
+	public String execute(Map<String, Object> data) throws Exception {
+		String method = (String)data.get("method");
+		
+		if(method.equals("GET")) {
+			BaljuService service = new BaljuService();
+			List<ProductVO> baljuchecklist = service.selectBaljuCheckList(14);
+			
+			for(ProductVO v : baljuchecklist) {
+				System.out.println(v);
+			}
+			
+			HttpServletRequest request = (HttpServletRequest)data.get("request");
+			request.setAttribute("baljuchecklist", baljuchecklist);
+		}
+		
+		return "/balju/balju.jsp";
+	}
+
+}
