9592d24bf9d79aab29aa1531a4bac0fead0a9635
teon98 <k55416325@gmail.com>
2023-04-19 17:42:15 +0900

    day3 태영 작업

---
+++ src/main/java/vo/ProductVO.java
@@ -0,0 +1,28 @@
+package vo;
+
+import java.sql.Timestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter @Setter @ToString
+public class ProductVO {
+
+	 private int product_code;
+	 private String subcategory_name;
+	 private int company_id;
+	 private String product_name;
+	 private int product_cost;
+	 private int product_price;
+	 private int product_stock;
+	 private int product_safety;
+	 private char product_status;
+	 private Timestamp product_regdate;
+	 private String manager_id;
+	 
+}
